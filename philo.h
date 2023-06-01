51db56e5577c57db90d939b38e846613a06946c3
Zveaga <98222905+Zveaga@users.noreply.github.com>
2023-06-01 18:17:51 +0200

    first steps

---
+++ philo.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   philo.h                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rares <rares@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/06/01 18:11:27 by rares         #+#    #+#                 */
+/*   Updated: 2023/06/01 18:17:00 by rares         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include<stdio.h>
+# include<unistd.h>
+# include<string.h>
+# include<stdlib.h>
+# include<sys/time.h>
+# include<stdlib.h>
+# include<pthread.h>
+
+
+
+
+
+
+#endif
